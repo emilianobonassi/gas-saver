5c5c38b7921795ced81b1c4581ea223732cc09df
Emiliano Bonassi <bonassi.emiliano@gmail.com>
2020-08-07 15:03:29 +0200

    Optimize GST2GasSaver

--- GST2GasSaver.sol
+++ GST2GasSaver.sol
@@ -13,22 +13,14 @@ contract GST2GasSaver {
         _;
 
         // Refund, thanks Mounir from Paraswap
-        uint256 MINT_BASE = 32254;
-        uint256 MINT_TOKEN = 36543;
-        uint256 FREE_BASE = 14154;
-        uint256 FREE_TOKEN = 6870;
-        uint256 REIMBURSE = 24000;
+        uint256 tokens = gasStart.sub(gasleft()).add(14154).div(uint256(24000).mul(2).sub(6870));
 
-        uint256 mintPrice = 1000000000;
-
-        uint256 tokens = gasStart.sub(gasleft()).add(FREE_BASE).div(REIMBURSE.mul(2).sub(FREE_TOKEN));
-
-        uint256 mintCost = MINT_BASE.add(tokens.mul(MINT_TOKEN));
-        uint256 freeCost = FREE_BASE.add(tokens.mul(FREE_TOKEN));
-        uint256 maxreimburse = tokens.mul(REIMBURSE);
+        uint256 mintCost = (tokens.mul(36543)).add(32254);
+        uint256 freeCost = (tokens.mul(6870)).add(14154);
+        uint256 maxreimburse = tokens.mul(24000);
 
         uint256 efficiency = maxreimburse.mul(tx.gasprice).mul(100).div(
-            mintCost.mul(mintPrice).add(freeCost.mul(tx.gasprice))
+            mintCost.mul(1000000000).add(freeCost.mul(tx.gasprice))
         );
 
         if (efficiency > 100) {
@@ -46,9 +38,7 @@ contract GST2GasSaver {
 
             IGST2 gasToken = IGST2(0x0000000000b3F879cb30FE243b4Dfee438691c04);
 
-            uint256 gasTokenBal = gasToken.balanceOf(sponsor);
-
-            if (tokensToFree > 0 && gasTokenBal >= tokensToFree) {
+            if (tokensToFree > 0 && gasToken.balanceOf(sponsor) >= tokensToFree) {
                 gasToken.freeFromUpTo(sponsor, tokensToFree);
             }
         }
